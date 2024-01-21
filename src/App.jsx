190cff9f629ed84b29f37cfec1e2821f7cf4ccda
TristanB <bonnal.tristan@hotmail.fr>
2024-01-22 00:51:58 +0100

    jsx part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import { useState } from 'react'
 
 function App() {
-    return ('Bonjour')
+    const style = {
+        color: 'green',
+        fontSize: '20px'
+    };
+
+    const firstName = 'John';
+    const name = 'Perez';
+    let counter = 0;
+    function handleClick(e) {
+        counter++;
+        console.log('clicked', counter, e);
+    }
+
+    return <h1
+        id="title"
+        style={style}
+        onClick={handleClick}
+    >
+        Hello, {firstName + ' ' + name}
+    </h1>;
 }
 
 export default App
