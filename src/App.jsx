2c83cba9cadda1ed3abae2b90e3483d7d82fe581
TristanB <bonnal.tristan@hotmail.fr>
2024-01-22 21:56:06 +0100

    arrays

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,39 @@ function App() {
 
     const firstName = 'John';
     const name = 'Perez';
+    const age = 25;
+
     let counter = 0;
+
+    const hobbies = ['music', 'sport', 'cinema'];
     function handleClick(e) {
         counter++;
         console.log('clicked', counter, e);
     }
 
-    return <h1
-        id="title"
-        style={style}
-        onClick={handleClick}
-    >
-        Hello, {firstName + ' ' + name}
-    </h1>;
+    return <>
+        <h1 id="title" style={style} onClick={handleClick}>
+            Hello, {firstName + ' ' + name}
+        </h1>
+
+        {age &&    // si age existe (true) alors on affiche le paragraphe
+            <p>Age: {age}</p>
+        }
+
+        <ul>
+            {hobbies.map((hobby, index) => {
+                return <li id={index} key={index}>{hobby}</li>
+            })}
+        </ul>
+
+        <footer>
+            <Footer />
+        </footer>
+    </>
+}
+
+function Footer() {
+    return ('Je suis le footer')
 }
 
 export default App
